97792edb6f63da1e136fb5f2a214b7ceec3fc4a2
bjterry99 <bjterry99@gmail.com>
2022-01-13 09:23:01 -0700

    misssion 3

---
+++ myFirstApp/Controllers/SubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myFirstApp.Controllers
+{
+    public class SubController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        public IActionResult Calc ()
+        {
+            return View();
+        }
+    }
+}
--- myFirstApp/wwwroot/js/js.js
+++ myFirstApp/wwwroot/js/js.js
@@ -3,26 +3,45 @@
     var grade = 0
     var letter
 
-    //calculate grade
-    grade = ((parseInt($("#assign").val()) * .55) + (parseInt($("#group").val()) * .05) + (parseInt($("#quiz").val()) * .1) + (parseInt($("#exam").val()) * .2) + (parseInt($("#intex").val()) * .1))
+    //check for values between 0 and 100
+    if (($("#assign").val() > 100) || ($("#assign").val() < 0)) {
+        alert("Please enter values between 0 and 100.")
+    }
+    else if (($("#group").val() > 100) || ($("#group").val() < 0)) {
+        alert("Please enter values between 0 and 100.")
+    }
+    else if (($("#quiz").val() > 100) || ($("#quiz").val() < 0)) {
+        alert("Please enter values between 0 and 100.")
+    }
+    else if (($("#exam").val() > 100) || ($("#exam").val() < 0)) {
+        alert("Please enter values between 0 and 100.")
+    }
+    else if (($("#intex").val() > 100) || ($("#intex").val() < 0)) {
+        alert("Please enter values between 0 and 100.")
+    }
+    else {
+        //calculate grade
+        grade = ((parseInt($("#assign").val()) * .55) + (parseInt($("#group").val()) * .05) + (parseInt($("#quiz").val()) * .1) + (parseInt($("#exam").val()) * .2) + (parseInt($("#intex").val()) * .1))
 
-    //determine grade level
-    if (grade >= 90) {
-        letter = 'A'
-    }
-    if (grade < 90 && grade >= 80) {
-        letter = 'B'
-    }
-    if (grade < 80 && grade >= 70) {
-        letter = 'C'
-    }
-    if (grade < 70 && grade >= 60) {
-        letter = 'D'
-    }
-    if (grade < 60) {
-        letter = 'F'
+        //determine grade level
+        if (grade >= 90) {
+            letter = 'A'
+        }
+        if (grade < 90 && grade >= 80) {
+            letter = 'B'
+        }
+        if (grade < 80 && grade >= 70) {
+            letter = 'C'
+        }
+        if (grade < 70 && grade >= 60) {
+            letter = 'D'
+        }
+        if (grade < 60) {
+            letter = 'F'
+        }
+
+        //print
+        alert("Your grade is: " + grade + "%\nLetter grade: " + letter);
     }
 
-    //print
-    alert("Your grade is: " + grade + "%\nLetter grade: " + letter);
 })
